199ad6785101e304cdb4ba81c5a89754376e88c5
NGANIZI <dannganizip@gmail.com>
2023-08-22 08:30:01 -0700

    bigner

---
+++ bas.py
@@ -0,0 +1,51 @@
+# name = input("What is your name? ")
+# print("Hello " + name)
+# DOB = input("what is your Date of Birth? ")
+# age = 2023 - int(DOB)
+# print(age)
+
+# #if statement
+# temperature = input("What is your temperature? ")
+
+# if (int(temperature) >= 60): {
+#     print("Normal")
+# }
+
+# else:
+#     {print("abnormal")
+#      }
+    
+# # Changing Weight into pounds
+# weight = int(input("Weight: "))
+# unit = input("(L)bs or (K)gs: ") 
+# if unit == "L":
+#     converted = weight * 0.45
+#     print(f"You are {converted} kilos")
+# else:
+#     converted= weight / 0.45
+#     print(f"You are {converted} pounds")
+
+
+# collect_value = 9
+# guess_start = 0
+# guess_end = 3
+# while guess_start < guess_end:
+#     Guess = int(input("Guess: "))
+#     guess_start +=1
+#     if Guess == collect_value:
+#         print("won")
+#         break
+#     else:
+#         print("You failed")
+    
+
+# x=1
+# while x<=5:
+#     print("*" *x)
+#     x =x+1
+# print("Done")
+
+# prices = [10,50,90]
+
+for price in [10,50,90]:
+    print(price)
